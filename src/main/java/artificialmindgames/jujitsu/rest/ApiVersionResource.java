7318035a48ebf1f6e5217b4f394c1eb1e3c228ce
aigames <artificialmindgames@gmail.com>
2013-12-21 17:05:23 +0000

    Use grizzly and jersey. builds and starts with trivial rest resource

--- src/main/java/artificialmindgames/jujitsu/rest/ApiVersionResource.java
+++ src/main/java/artificialmindgames/jujitsu/rest/ApiVersionResource.java
@@ -1,4 +1,4 @@
-package psychological.jujitsu.rest;
+package artificialmindgames.jujitsu.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
---
+++ src/main/java/artificialmindgames/jujitsu/servlet/JujitsuMain.java
@@ -0,0 +1,36 @@
+package artificialmindgames.jujitsu.servlet;
+
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import artificialmindgames.jujitsu.rest.ApiVersionResource;
+
+
+public class JujitsuMain {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(JujitsuMain.class);
+	
+    public static void main(String[] args) throws Exception{
+    	
+    	String port = System.getenv("PORT");
+    	if (port == null) {
+    		port = "9998";
+    	}
+    	
+    	LOGGER.info("starting HTTP server on port {}", port);
+    	
+    	URI baseUri = UriBuilder.fromUri("http://localhost/").port(9998).build();
+        ResourceConfig config = new ResourceConfig(ApiVersionResource.class);
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, config);
+    	
+        server.start();
+        Thread.currentThread().join();
+    }
+}
